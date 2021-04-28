54dbf0c2b942e7050ca1be61a5a62276678a6476
AlbertoFreije <freicartobe@gmail.com>
2021-04-28 12:29:49 +0200

    SDI-2021-106-SW-11.2- Servicio SOAP Cliente

---
+++ sdi2021-106-soap-cliente/src/main/java/com/uniovi/Sdi2021106SoapClienteApplication.java
@@ -0,0 +1,14 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021106SoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021106SoapClienteApplication.class, args);
+		
+	}
+
+}
